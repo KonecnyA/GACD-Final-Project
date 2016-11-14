1008684c617881e59be52a645f79cf67b1fb8a9d
KonecnyA <Andrew.F.Konecny@outlook.com>
2016-11-14 13:38:18 -0500

    Removed Function template decided not to add another Function

--- GACD project functions.R
+++ GACD project functions.R
@@ -3,9 +3,6 @@ load_data_frame <- function(file_path, file_name, colnames) {
   read.table(paste(file_path, file_name, ".txt", sep = ""), col.names = colnames, check.names = FALSE)
 }
 
-## Purpose of
-
-
 ## Purpose of this function is to take a data frame, select a range of columns, write a csv file and verify the written file.
 write_and_check <- function(data_frame, column_range_start, column_range_end, file_path, file_name) {
   ## Create a test check sum - Before
